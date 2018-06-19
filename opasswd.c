ff232786bb483dcf06ab344b270da47aa446ffd0
Christian Lindig <christian.lindig@citrix.com>
2018-06-19 17:33:08 +0100

    Use re-entrant functions, realloc(), bind to OCaml
    * getpwd: avoid race, try shadow password first
    * create unique temp files
    * use re-entrant functions to access password entries
    * set permission and owner of temp files
    * provide OCaml bindings
    * use Valgrind
    
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

--- opasswd.c
+++ opasswd.c
@@ -1,215 +1,347 @@
-
 /*
- * A first shot at replacing 
- * https://github.com/xapi-project/ocaml-opasswd.git
- * with a C library and a minimal interface to OCaml.
+ * Copyright (C) Citrix Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
+#include <errno.h>
+#include <malloc.h>
 #include <pwd.h>
 #include <shadow.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-
-#include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
-#include <malloc.h>
-#include <unistd.h>
-#include <errno.h>
 
-/* getpw: set pw to the encrypted password for user and return 0 on
- * success. The memory pointed to by pw must be freed by the caller.
- *
- * On failure, no memory is allocated and errno != 0 is returned.
+#ifndef MAIN
+#include <caml/alloc.h>
+#include <caml/mlvalues.h>
+#include <caml/fail.h>
+#include <caml/callback.h>
+#include <caml/memory.h>
+#endif
+
+#define DEVELOPMENT
+
+#ifdef DEVELOPMENT
+#define ETC_PASSWD   "passwd"
+#define TMP_PASSWD   "passwd.XXXXXX"
+#define ETC_SPASSWD  "shadow"
+#define TMP_SPASSWD  "shadow.XXXXXX"
+#else
+#define ETC_PASSWD   "/etc/passwd"
+#define TMP_PASSWD   "/etc/passwd.XXXXXX"
+#define ETC_SPASSWD  "/etc/shadow"
+#define TMP_SPASSWD  "/etc/shadow.XXXXXX"
+#endif
+
+#define BUFLEN 4096
+/*
+ * getpw: return the encrypted password for the user. This memory must
+ * be freed by the caller. On error and when no password entry is found,
+ * set errno and return NULL.
  */
-static int
-getpw(char *user, char **pw)
-{
 
-  if (access(SHADOW, R_OK) == 0) {
-    struct spwd *spw;
-    spw = getspnam(user);
-    if (!spw) {
-      *pw = NULL;
-      return errno;
-    }
-    *pw = strdup(spw->sp_pwdp);
-  } else {
-    struct passwd *pwd;
-    pwd = getpwnam(user);
-    if (!pwd) {
-      *pw = NULL;
-      return errno;
-    }
-    *pw = strdup(pwd->pw_passwd);
-  }
-  return 0;
+static char    *
+getpwd(const char *user)
+{
+    struct spwd     spw,
+                   *sp;
+    struct passwd   pwd,
+                   *pw;
+    char            buf[BUFLEN];
+
+    if (getspnam_r(user, &spw, buf, BUFLEN, &sp) == 0)
+        return strdup(sp->sp_pwdp);
+    if (getpwnam_r(user, &pwd, buf, BUFLEN, &pw) == 0)
+        return strdup(pw->pw_passwd);
+    return NULL;
 }
 
-#define ETC_PASSWD "passwd"
-#define TMP_PASSWD "passwd.tmp"
-
-/* setpwd
- * set password entry for a user
+/*
+ * setpwd set password entry for a user
  */
 
 static int
-setpwd(char *user, char* cipher)
+setpwd(char *user, char *password)
 {
 
-  struct passwd *pwd;
-  FILE *tmp;
+    struct passwd   pwd,
+                   *pw;
+    char            buf[BUFLEN];
+    int             tmp;
+    FILE           *tmp_file;
+    static char     tmp_name[] = TMP_PASSWD;
+    struct stat     statbuf;
+    int             rc;
 
-  umask(0033);
-  tmp = fopen(TMP_PASSWD, "w");
-  if (!tmp) {
-    return errno;
-  } 
-  
-  setpwent();
-  while (pwd = getpwent()) {
-    if(!strcmp(user, pwd->pw_name)) {
-      char *old = pwd->pw_passwd;
-      pwd->pw_passwd = cipher;
-      putpwent(pwd, tmp);
-      pwd->pw_passwd = old;
-    } else {
-      putpwent(pwd, tmp);
+    tmp = mkstemp(tmp_name);
+    if (!tmp)
+        return errno;
+    if (stat(ETC_PASSWD, &statbuf) != 0)
+        return errno;
+    if (fchown(tmp, statbuf.st_uid, statbuf.st_gid) != 0)
+        return errno;
+    if (fchmod(tmp, statbuf.st_mode) != 0)
+        return errno;
+    tmp_file = fdopen(tmp, "w");
+    if (!tmp_file)
+        return errno;
+
+    setpwent();
+    while (1) {
+        rc = getpwent_r(&pwd, buf, BUFLEN, &pw);
+        if (rc != 0)
+            break;
+        if (!strcmp(user, pw->pw_name))
+            pw->pw_passwd = password;
+        putpwent(pw, tmp_file);
     }
-  }
-  endpwent();
+    endpwent();
 
-  fclose(tmp);
-  if (!rename(TMP_PASSWD,ETC_PASSWD)) {
-    return errno;
-  }
-  
-  return 0;
+    fclose(tmp_file);
+    if (rc != ENOENT)
+        return rc;
+    if (rename(tmp_name, ETC_PASSWD) != 0)
+        return errno;
+    return 0;
 }
 
-#define ETC_SPASSWD "shadow"
-#define TMP_SPASSWD "shadow.tmp"
 
-/* setspw
- * set a shadow password for a user
+/*
+ * setspw - set shadow password for user
  */
 static int
-setspw(char *user, char* cipher)
+setspw(char *user, char *password)
 {
 
-  struct spwd *pwd;
-  FILE *tmp;
+    struct spwd     spw,
+                   *sp;
+    char            buf[BUFLEN];
+    int             tmp;
+    FILE           *tmp_file;
+    static char     tmp_name[] = TMP_SPASSWD;
+    struct stat     statbuf;
+    int             rc;
 
-  umask(0037);
-  tmp = fopen(TMP_SPASSWD, "w");
-  if (!tmp) {
-    return errno;
-  } 
-  
-  setspent();
-  while (pwd = getspent()) {
-    if(!strcmp(user, pwd->sp_namp)) {
-      char *old = pwd->sp_pwdp;
-      pwd->sp_pwdp = cipher;
-      putspent(pwd, tmp);
-      pwd->sp_pwdp = old;
-    } else {
-      putspent(pwd, tmp);
+    tmp = mkstemp(tmp_name);
+    if (!tmp)
+        return errno;
+    if (stat(ETC_SPASSWD, &statbuf) != 0)
+        return errno;
+    if (fchown(tmp, statbuf.st_uid, statbuf.st_gid) != 0)
+        return errno;
+    if (fchmod(tmp, statbuf.st_mode) != 0)
+        return errno;
+    tmp_file = fdopen(tmp, "w");
+    if (!tmp_file)
+        return errno;
+
+    setspent();
+    while (1) {
+        rc = getspent_r(&spw, buf, BUFLEN, &sp);
+        if (rc != 0)
+            break;
+        if (!strcmp(user, sp->sp_namp))
+            sp->sp_pwdp = password;
+        putspent(sp, tmp_file);
     }
-  }
-  endspent();
+    endspent();
 
-  fclose(tmp);
-  if (!rename(TMP_SPASSWD,ETC_SPASSWD)) {
-    return errno;
-  }
-  
-  return 0;
+    fclose(tmp_file);
+    if (rc != ENOENT)
+        return rc;
+    if (rename(tmp_name, ETC_SPASSWD) != 0)
+        return errno;
+    return 0;
 }
 
-/* unshadow
- * return /etc/passwd as a string but with password entries from the
- * shadow password file if it has a corresponding entry.
- * The memory returned by this function must be freed by the caller.
+/*
+ * unshadow - return /etc/passwd as a string but with password entries from
+ * the shadow password file if it has a corresponding entry. The memory
+ * returned by this function must be freed by the caller.
  */
-char *
+static char    *
 unshadow(void)
 {
-  struct spwd *spw;
-  struct passwd *pwd;
-  char *buf, *cur, *end;
-  int bufsize = 64 * 1024;
+    struct spwd     spw,
+                   *sp;
+    struct passwd   pwd,
+                   *pw;
+    char            pwbuf[BUFLEN];
+    char            spbuf[BUFLEN];
 
-  buf = malloc(bufsize);
-  if (!buf) {
-    return NULL;
-  }
-  cur = buf;
-  end = buf + bufsize;
+    char           *buf;
+    int             size,
+                    cur;
 
-  setpwent();
-  while (pwd = getpwent()) {
-    spw = getspnam(pwd->pw_name);
-    cur += snprintf(cur, end-cur,"%s:%s:%d:%d:%s:%s\n",
-        pwd->pw_name,
-        spw ? spw->sp_pwdp : pwd->pw_passwd,
-        pwd->pw_uid,
-        pwd->pw_gid,
-        pwd->pw_gecos,
-        pwd->pw_dir,
-        pwd->pw_shell);
-  }
-  endpwent();
-  if (cur >= end) {
-    free(buf);
-    buf = NULL;
-  }
-  return buf;
+    size = 1024;
+    cur = 0;
+    buf = malloc(size);
+    if (!buf) {
+        return NULL;
+    }
+
+    setpwent();
+    while (1) {
+        char            tmp[BUFLEN];
+        int             written;
+
+        if (getpwent_r(&pwd, pwbuf, BUFLEN, &pw) != 0)
+            break;
+        getspnam_r(pw->pw_name, &spw, spbuf, BUFLEN, &sp);
+
+        written = snprintf(tmp, BUFLEN, "%s:%s:%d:%d:%s:%s\n",
+                           pw->pw_name,
+                           sp ? sp->sp_pwdp : pw->pw_passwd,
+                           pw->pw_uid,
+                           pw->pw_gid,
+                           pw->pw_gecos, pw->pw_dir, pw->pw_shell);
+        if (written >= BUFLEN) {
+            endpwent();
+            free(buf);
+            return NULL;
+        }
+        while (cur + written > size) {
+            size = size << 1;
+            buf = realloc(buf, size);
+            if (!buf) {
+                endpwent();
+                return NULL;
+            }
+        }
+        strncpy(buf + cur, tmp, size - cur);
+        cur += written;
+    }
+    endpwent();
+
+    return buf;
 }
 
-int main(int argc, char**argv)
+#ifndef MAIN
+CAMLprim        value
+caml_getpwd(value caml_user)
 {
-  int rc;
-  char *pw;
-  char *buf;
+    CAMLparam1(caml_user);
+    char           *user;
+    char           *passwd;
+    CAMLlocal1(pw);
 
-  switch (argc) {
+    user = String_val(caml_user);
+    passwd = getpwd(user);
+    if (passwd == NULL)
+        caml_raise_not_found();
+
+    pw = caml_copy_string(passwd);
+    free(passwd);
+    CAMLreturn(pw);
+}
+
+CAMLprim        value
+caml_setpwd(value caml_user, value caml_password)
+{
+    CAMLparam2(caml_user, caml_password);
+    char           *user,
+                   *password;
+    int             rc;
+
+    user = String_val(caml_user);
+    password = String_val(caml_password);
+    rc = setpwd(user, password);
+    CAMLreturn(Val_int(rc));
+}
+
+CAMLprim        value
+caml_setspw(value caml_user, value caml_password)
+{
+    CAMLparam2(caml_user, caml_password);
+    char           *user,
+                   *password;
+    int             rc;
+
+    user = String_val(caml_user);
+    password = String_val(caml_password);
+    rc = setspw(user, password);
+    CAMLreturn(Val_int(rc));
+}
+
+CAMLprim        value
+caml_unshadow(void)
+{
+    CAMLparam0();
+    char           *passwords;
+    CAMLlocal1(str);
+
+    passwords = unshadow();
+    if (passwords == NULL)
+        caml_raise_not_found();
+
+    str = caml_copy_string(passwords);
+    free(passwords);
+    CAMLreturn(str);
+}
+#endif
+
+#ifdef MAIN
+int
+main(int argc, char **argv)
+{
+    int             rc;
+    char           *pw;
+    char           *buf;
+
+    switch (argc) {
     case 1:
-      buf = unshadow();
-      if (buf) {
-        puts(buf);
-        free(buf);
-      } else {
-        fprintf(stderr,"can't unshadow\n");
-      }
-      break;
+        buf = unshadow();
+        if (buf) {
+            puts(buf);
+            free(buf);
+        } else {
+            fprintf(stderr, "can't unshadow\n");
+        }
+        break;
 
-    case 2: 
-      if (getpw(argv[1],&pw) == 0) {
-        printf("%s: %s\n", argv[1], pw);
-        free(pw);
-        rc = 0;
-      } else {
-        fprintf(stderr,"can't find entry for %s\n", argv[1]);
-        rc = 1;
-      }
-      break;
-            
+    case 2:
+        pw = getpwd(argv[1]);
+        if (pw) {
+            printf("%s: %s\n", argv[1], pw);
+            free(pw);
+            rc = 0;
+        } else {
+            fprintf(stderr, "can't find entry for %s\n", argv[1]);
+            rc = 1;
+        }
+        break;
+
     case 3:
-      rc = setspw(argv[1],argv[2]);
-      if (rc != 0) {
-        fprintf(stderr,"error setting password: %d\n", rc);
-        rc = 1;
-      }
-      break;
+        rc = setpwd(argv[1], argv[2]);
+        if (rc != 0) {
+            fprintf(stderr, "error setting password: %d\n", rc);
+            rc = 1;
+            break;
+        }
+        rc = setspw(argv[1], argv[2]);
+        if (rc != 0) {
+            fprintf(stderr, "error setting shadow password: %d\n", rc);
+            rc = 1;
+            break;
+        }
+        break;
 
     default:
-      fprintf(stderr,"usage: opasswd user [password]\n");
-      rc = 1;
-  }
-  exit(rc);
+        fprintf(stderr, "usage: opasswd user [password]\n");
+        rc = 1;
+    }
+    exit(rc);
 }
-
-
+#endif
---
+++ pwd.ml
@@ -0,0 +1,5 @@
+
+external getpwd :   string -> string              = "caml_getpwd"
+external setpwd :   string -> string -> int       = "caml_setpwd"
+external setspw :   string -> string -> int       = "caml_setspw"
+external unshadow : unit   -> string              = "caml_unshadow"
